b45e6cfa2459e868d99b524bc59af467790bb449
devgramop <168399896+devgramop@users.noreply.github.com>
2024-06-06 12:11:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/kxunal1834/SOME",
+    "https://github.com/devgramop/SOME",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
